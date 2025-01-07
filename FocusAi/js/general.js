9c849d749a2a1cd4c613c636ef6c741d5b698393
koisol <koisol@yahoo.co.uk>
2025-01-07 01:30:18 +0000

    Files checked in

---
+++ FocusAi/js/general.js
@@ -0,0 +1,12 @@
+﻿window.scrollToSection = function (sectionId) {
+        const section = document.getElementById(sectionId);
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+        }
+}
+
+window.toggleFAQ = function (faqItem)
+{
+    const answer = faqItem.querySelector('.faq-answer');
+    answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
+}
